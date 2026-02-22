96ba0e9db4d63180337ad89da1b4629e690e85fc
Thorsten Diekhof <kontakt@thorsten-diekhof.de>
2026-02-22 08:55:03 +0100

    Create Maven Hallo-Welt Java project with test

---
+++ clean-code-workshop/src/main/java/de/workshop/App.java
@@ -0,0 +1,12 @@
+package de.workshop;
+
+public class App {
+
+    public static String getGreeting() {
+        return "Hallo Welt";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+}
